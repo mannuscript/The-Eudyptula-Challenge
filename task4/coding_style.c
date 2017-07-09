214f3e83ccc1c3a44cf151c18819868eb42be183
Mannu <mannu1200@gmail.com>
2017-07-09 10:05:59 +0700

    Syntax corrections for task 4 c file

--- task4/coding_style.c
+++ task4/coding_style.c
@@ -1,6 +1,6 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <asm/delay.h>
+#include <linux/delay.h>
 #include <linux/slab.h>
 
 int do_work(int *my_int, int retval)
@@ -14,7 +14,7 @@ int do_work(int *my_int, int retval)
 
 	if (y < 10)
 		// That was a long sleep, tell userspace about it
-		printk("We slept a long time!");
+		printk(KERN_DEBUG, "We slept a long time!");
 
 	z = x * y;
 
